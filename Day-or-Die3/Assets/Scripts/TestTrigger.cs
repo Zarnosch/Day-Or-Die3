53e5767429120c1c76c88558bb869ffe7664e6d7
Saxton <saxton217@gmx.de>
2016-06-26 04:31:27 +0200

    New feature changeScene by collision

---
+++ Day-or-Die3/Assets/Scripts/TestTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TestTrigger : MonoBehaviour {
+
+    void OnTriggerEnter(Collider col)
+    {
+        string aim = col.gameObject.name;
+        if(aim == "Player")
+        {
+            SceneManager.LoadScene(gameObject.GetComponent<Aim>().getScene());
+        }
+    }
+}
