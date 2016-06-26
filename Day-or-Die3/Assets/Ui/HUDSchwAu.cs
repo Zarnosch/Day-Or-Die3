196b92684b6815191ee8718a710c17ac48fa1bc4
Tamara Rautenstengel <tamara.rautenstengel@farafin.de>
2016-06-26 03:52:46 +0200

    Szenen

---
+++ Day-or-Die3/Assets/Ui/HUDSchwAu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDSchwAu : MonoBehaviour {
+
+	public GameObject player;
+	int maxau;
+	int maxwa;
+
+	void Start()
+	{
+		maxau = gameObject.GetComponent<Stats>().getAusdauerMay();
+		maxwa = gameObject.GetComponent<Stats>().getWasserMax();
+	}
+
+
+	void OnGUI()
+	{
+		int currau = gameObject.GetComponent<Stats> ().ausdauer;
+		int currwa = gameObject.GetComponent<Stats> ().wasser;
+
+		float prowa = currwa / maxwa;
+		float proau = currau / maxau;
+
+
+		GUI.Label (new Rect(Screen.height-200, 50,50,50), "Ausdauer left: ");
+
+		GUI.Box (new Rect (Screen.height - 175,50,200*proau,50), maxau-currau);
+
+		GUI.Label (new Rect(Screen.height-100, 50, 50,50), "Schweiß left: ");
+
+		GUI.Box (new Rect (Screen.height - 75,50,200*prowa,50),maxwa-currwa);
+
+
+	}
+
+}
---
+++ Day-or-Die3/Assets/Ui/HelperGUI.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelperGUI : MonoBehaviour {
+
+	void onGUI()
+	{
+		GUI.Button (new Rect (10, 10, 100, 25), "Fuck ME!");
+	}
+}
