1fbeda48a7968d854d9f28d1e472cdc525b83336
Tamara Rautenstengel <tamara.rautenstengel@farafin.de>
2016-06-26 13:49:33 +0200

    Texture + BUTTONS

---
+++ Day-or-Die3/Assets/Ui/HUDButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDButton : MonoBehaviour {
+
+	public Texture2D e_txt;
+	public Texture2D wasd_txt;
+	public Texture2D f_txt;
+	public Texture2D shift_txt;
+	public Texture2D leer_txt;
+	public Texture2D foto_txt;
+	public Texture2D esc_txt;
+
+
+}
